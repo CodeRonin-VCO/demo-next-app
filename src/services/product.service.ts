6d0e90f06dd36dcb634f6c4fa93e347f71d98f05
Vincent <vincentcss16@gmail.com>
2025-09-10 10:43:27 +0200

    Ajout méthodes (stock, isFood, outOfStock & add)

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -3,17 +3,6 @@ import { Product, ProductListItem } from "@/@types/product";
 // ?Remarque de construction fichier : juste mettre des throw error dans chaque méthode pour voir si ça passe avant de coder tous le code
 
 const productService = {
-    getById: async (id: number): Promise<Product> => {
-        const res = await fetch(`http://localhost:4224/products/${id}`);
-
-        if (!res.ok) {
-            throw new Error("Product not found");
-        }
-
-        const data = await res.json();
-
-        return data;
-    },
     getAll: async (): Promise<ProductListItem[]> => {
         const res = await fetch(`http://localhost:4224/products`);
 
@@ -25,6 +14,58 @@ const productService = {
 
         return data;
     },
+    getById: async (id: number): Promise<Product> => {
+        const res = await fetch(`http://localhost:4224/products/${id}`);
+
+        if (!res.ok) {
+            throw new Error("Product not found");
+        }
+
+        const data = await res.json();
+
+        return data;
+    },
+    getProductMostInStock: async (): Promise<ProductListItem[]> => {
+        const res = await fetch(`http://localhost:4224/products?_sort=stock&_order=desc`);
+        if (!res.ok) {
+            throw new Error("Product not found");
+        }
+        const data: Product[] = await res.json();
+        const maxStock = data[0].stock;
+        const datas = data.filter(e => e.stock === maxStock);
+
+        return datas;
+
+    },
+    getCountProduct: async (isFood: boolean): Promise<number> => {
+        const res = await fetch(`http://localhost:4224/products?isFood=${isFood}`);
+        const data: Product[] = await res.json();
+
+        return data.length;
+    },
+    getOutOfStock : async (): Promise<ProductListItem[]> => {
+        const res = await fetch(`http://localhost:4224/products?stock=0`);
+        const data: Product[] = await res.json();
+
+        return data;
+    },
+    add: async (product: Product): Promise<number> => {
+        const res: Response = await fetch(`http://localhost:4224/products`, {
+            method: "POST",
+            body: JSON.stringify(product),
+            headers: {
+                "Content-type": "application/json"
+            }
+        });
+
+        if (!res.ok) {
+            throw new Error("Failed to add product");
+        }
+
+        const data: Product = await res.json();
+
+        return data.id;
+    }
 }
 
 export default productService;
